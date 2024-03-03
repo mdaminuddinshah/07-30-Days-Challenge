3a1524f900826f311013db7b87158eb22e17056a
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-03 23:37:58 +0800

    change position

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-fetch('https://api.pexels.com/v1/search?query=tigers',{
-    headers: {
-        'Authorization' : 'iJ9aguah6tppfkovzqCfrztJRXPDGvYBfbN4kOhefFzFXdbJMK8oi5aZ'
-    }
-})
-    .then(res => res.json())
-    .then(data => {
-        const gambar = data.photos[1].src.small;
-        document.getElementById('gambar').src = gambar
+
+function gambar(){
+    const divtag = document.getElementById('app');
+    const apiElem = 'https://api.pexels.com/v1/search?query=bicycle&per_page=15';
+
+    fetch(apiElem, {
+        headers: {
+            'Authorization': 'iJ9aguah6tppfkovzqCfrztJRXPDGvYBfbN4kOhefFzFXdbJMK8oi5aZ'
+        }
     })
+    .then(res => res.json())
+    .then(data => data.photos.map(elem => (
+        `<img src="${elem.src.large}" style="width: 200px; height: 200px;"/>`
+    )))
+    .then(result => 
+        divtag.innerHTML = result)
+}
+
+gambar();
