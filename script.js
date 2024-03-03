46e993129d37eebb1e3015ae3c8fa7764817013a
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-04 01:23:43 +0800

    change api

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchGambar = document.getElementById('searchGambar');
 function gambar(){
     
     const divtag = document.getElementById('app');
-    const apiElem = 'https://api.pexels.com/v1/search?query=bicycle&per_page=15';
+    const apiElem = 'https://api.pexels.com/v1/search?query=cat&per_page=15';
     fetch(apiElem, {
         headers: {
             'Authorization': 'iJ9aguah6tppfkovzqCfrztJRXPDGvYBfbN4kOhefFzFXdbJMK8oi5aZ'
